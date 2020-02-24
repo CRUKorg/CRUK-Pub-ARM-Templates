b406d6e788be5c66a6d9434ba5ef95655a118a33
Joff - CRUK <59734122+Joff-CRUK@users.noreply.github.com>
2020-02-24 12:32:53 +0000

    Creating Set-AzureVMNIC

---
+++ Config/Set-AzureVMNIC.ps1
@@ -0,0 +1,26 @@
+#Powershell script to set up NIC on Azure VM
+#Region IP settings
+#Vars
+$nicToSet = Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias 'Ethernet'
+$nicAlias = ($nicToSet).InterfaceAlias
+$newIP = ((Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -match '10.' }).IPAddress)
+$newSubnet = ((Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -match '10.' }).PrefixLength)
+$newGateway = ((Get-NetIPConfiguration).IPv4DefaultGateway).nexthop
+#endregion
+
+#Region DNS Settings
+#Vars
+$dnsIndex = (Get-NetAdapter).ifIndex
+$DNSServers = ((Get-NetIPConfiguration).dnsserver ).serveraddresses | Where-Object { $_ -match '10.' } 
+$newDNSServers = $DNSServers -join ','
+#endregion
+
+#Remove Current
+Remove-NetIPAddress -InterfaceAlias $nicAlias -Confirm:$false
+Remove-NetRoute -InterfaceAlias $nicAlias -Confirm:$false
+
+#Set New
+New-NetIPAddress -InterfaceAlias $nicAlias -IPAddress $newIP -PrefixLength $newSubnet -DefaultGateway $newGateway
+Set-DnsClientServerAddress -InterfaceIndex $dnsIndex -ServerAddresses $newDNSServers
+
+#endregion
